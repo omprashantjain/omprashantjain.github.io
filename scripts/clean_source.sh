fabfc8329cd43e2f70533c10fe0629cf3db1ce1f
Prashant Jain <prashant.jain@mubble.in>
2017-01-15 15:57:37 +0530

    BI: Deleted extra stuff and fixed basic info.

--- scripts/clean_source.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf /tmp/var/www/codedeploy-ysharma-test/
-mkdir -p /tmp/var/www/codedeploy-ysharma-test
-chmod 755 /tmp/var/www/codedeploy-ysharma-test
--- scripts/copy_to_html.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp -rf /tmp/var/www/codedeploy-ysharma-test/* /var/www/cds/
-chmod -R 755 /var/www/cds
